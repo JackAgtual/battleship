802a2124db3f1a3a683a8231f6fdc2c74e53dcb8
Jack Agtual <jaagtual@gmail.com>
2023-03-08 22:45:42 -0800

    Adding unit test for isSunk() method on Ship

---
+++ src/tests/ship.test.js
@@ -0,0 +1,25 @@
+const Ship = require('../ship.js')
+
+describe('Ship', () => {
+
+    it('should not sink if not hit', () => {
+        const shipWithNoHits = Ship({ length: 2 })
+        expect(shipWithNoHits.isSunk()).toBe(false)
+    })
+
+    it('should not sink if hits < length', () => {
+        const shipWithLessHitsThanLenght = Ship({ length: 4, numHits: 2 })
+        expect(shipWithLessHitsThanLenght.isSunk()).toBeFalsy()
+    })
+
+    it('should sink when hits == length', () => {
+        const shipWithSameHitsAsLength = Ship({ length: 10, numHits: 10 })
+        expect(shipWithSameHitsAsLength.isSunk()).toBeTruthy()
+    })
+
+    it('should sink when hits > length', () => {
+        const shipWithMoreHitsThanLength = Ship({ length: 6, numHits: 7 })
+        expect(shipWithMoreHitsThanLength.isSunk()).toBeTruthy()
+    })
+})
+
