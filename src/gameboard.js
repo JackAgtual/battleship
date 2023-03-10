9a9b58946c7220273b34ecc16ab8d4afd8faf8af
Jack Agtual <jaagtual@gmail.com>
2023-03-09 22:05:44 -0800

    Gambeboard able to add ship if in bounds

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,8 +2,36 @@ function Gameboard() {
     const _size = 10
     const _grid = Array(_size).fill().map(() => Array(_size).fill(false))
 
-    const placeShip = (ship, origin, direction) => {
+    const _directionIsValid = direction => ['up', 'down', 'left', 'right'].includes(direction.toLowerCase())
 
+    const _shipIsInBounds = (shipLength, origin, direction) => {
+        return true
+    }
+
+    const placeShip = (ship, origin, direction) => {
+        direction = direction.toLowerCase()
+        const [row, col] = origin
+        const shipLength = ship.getLength()
+
+        if (!_directionIsValid(direction) || !_shipIsInBounds(shipLength, origin, direction)) return null
+
+        for (let i = 0; i < shipLength; i++) {
+            switch (direction) {
+                case 'up':
+                    _grid[row - i][col] = true
+                    break
+                case 'down':
+                    _grid[row + i][col] = true
+                    break
+                case 'left':
+                    _grid[row][col - i] = true
+                    break
+                case 'right':
+                    _grid[row][col + i] = true
+                    break
+            }
+        }
+        return _grid
     }
 
     const receiveAttack = coordinate => {
