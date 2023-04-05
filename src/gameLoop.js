d48bacadc3051fca8c3fe727308506a33d5541f9
Jack Agtual <jaagtual@gmail.com>
2023-04-04 21:33:35 -0700

    Adding GameLoop and importing

---
+++ src/gameLoop.js
@@ -0,0 +1,10 @@
+export default function GameLoop(PlayerGameboard, ComputerGameboard, DomController) {
+
+    const startGame = () => {
+        console.log('game started')
+    }
+
+    return {
+        startGame
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './styles.css';
 import DomController from './domController';
 import Gameboard from './gameboard';
+import GameLoop from './gameLoop';
 
 const playerGameboard = Gameboard()
 const computerGameboard = Gameboard()
 computerGameboard.randomlyPlaceShips()
 const domController = DomController()
 domController.init(playerGameboard, computerGameboard)
+const gameLoop = GameLoop(playerGameboard, computerGameboard, domController)
+gameLoop.startGame()
